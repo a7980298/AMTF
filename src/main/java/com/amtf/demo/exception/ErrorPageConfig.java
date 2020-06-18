6c4372eb27a50a2f83c604a8fce8d9d7b01d8da6
wyx <email@example.com>
2020-06-18 21:58:04 +0800

    404页面创建

---
+++ src/main/java/com/amtf/demo/exception/ErrorPageConfig.java
@@ -0,0 +1,29 @@
+package com.amtf.demo.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorPageConfig implements ErrorController {
+	@Autowired
+	HttpServletRequest request;
+
+	@Override
+	@RequestMapping("/error")
+	public String getErrorPath() {
+		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+		switch (statusCode) {
+		case 404:
+			return "/404";
+		case 400:
+			return "/400";
+		default:
+			return "/500";
+		}
+	}
+
+}
---
+++ src/main/java/com/amtf/demo/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.amtf.demo.exception;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotFoundException {
+
+}
