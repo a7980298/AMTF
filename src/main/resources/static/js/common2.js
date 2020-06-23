894a4707feaa1dc35d4674bd397718927814527a
wyx <email@example.com>
2020-06-23 20:31:45 +0800

    jq正则修改

--- src/main/resources/static/js/common2.js
+++ src/main/resources/static/js/common2.js
@@ -1,33 +1,43 @@
 $(function(){
 	EL_Number();
+	EL_NumberOrLetter();
+	EL_WordAndSymbol();
 })
 
 //数字入力check
-function EL_Number(){
-	$('.el_number').change(function(){
-		var el = /^[0-9]*$/;
-		$('.el_number').bind('input propertychange',function(){
-			var _val = $(this).val();
-			var bol = el.test(_val);
-			if (!bol) {
-				$(this).next().show();
-				$(this).val(_val.substr(0,_val.length-1));
-			}
-		});
+function EL_Number() {
+	var el = /^[0-9]*$/;
+	$('.el_number').bind('input propertychange',function() {
+		var _val = $(this).val();
+		var bol = el.test(_val);
+		if (!bol) {
+			$(this).next().show();
+			$(this).val(_val.substr(0,_val.length-1));
+		}
 	});
 }
 //数字和字母(不区分大小写)入力check
-function EL_NumberOrLetter(){
-	$('.el_numberorletter').change(function(){
-		var el = /^[A-Za-z]+$ /;
-		$('.el_numberorletter').bind('input propertychange',function(){
-			var _val = $(this).val();
-			var bol = el.test(_val);
-			if (!bol) {
-				$(this).next().show();
-				$(this).val(_val.substr(0,_val.length-1));
-			}
-		});
+function EL_NumberOrLetter() {
+	var el = /^[A-Za-z0-9]*$/;
+	$('.el_numberorletter').bind('input propertychange',function() {
+		var _val = $(this).val();
+		var bol = el.test(_val);
+		if (!bol) {
+			$(this).next().show();
+			$(this).val(_val.substr(0,_val.length-1));
+		}
 	});
-	
+}
+
+//只能输入汉字和标点符号check
+function EL_WordAndSymbol() {
+	var el = /^[\u4E00-\u9FA5]+$/;
+	$('.el_word').bind('input propertychange',function() {
+		var _val = $(this).val();
+		var bol = el.test(_val);
+		if (!bol) {
+			$(this).next().show();
+			$(this).val(_val.substr(0,_val.length-1));
+		}
+	});
 }
