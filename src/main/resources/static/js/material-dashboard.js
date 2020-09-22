672b429afe292c32531860ced677f889022e1b70
wyx <dhc@DESKTOP-JUIGH46>
2020-09-22 09:50:35 +0900

    1

--- src/main/resources/static/js/material-dashboard.js
+++ src/main/resources/static/js/material-dashboard.js
@@ -257,17 +257,17 @@ md = {
     }
   },
 
-  showNotification: function(from, align) {
+  showNotification: function(from, align, typeindex, message) {
     type = ['', 'info', 'danger', 'success', 'warning', 'rose', 'primary'];
 
     color = Math.floor((Math.random() * 6) + 1);
 
     $.notify({
       icon: "add_alert",
-      message: "Welcome to <b>Material Dashboard Pro</b> - a beautiful admin panel for every web developer."
+      message: message
 
     }, {
-      type: type[color],
+      type: type[typeindex],
       timer: 3000,
       placement: {
         from: from,
