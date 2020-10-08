9c8b6a0d4f62f765f47552660726550c1f747e89
wyx <email@example.com>
2020-10-08 20:14:46 +0800

    1

--- src/main/java/com/amtf/demo/serviceImpl/CommonServiceImpl.java
+++ src/main/java/com/amtf/demo/serviceImpl/CommonServiceImpl.java
@@ -28,21 +28,29 @@ public class CommonServiceImpl {
 	// 限制登录人数
 	public void stopLogin() {
 		LogInFo loginfoget = new LogInFo();
+		// 获取当前用户
 		loginfoget = ParameterUtil.getSession();
 		String redis_key = redisUtils.get("redis_key");
+		// 判断是否有用户登录中
 		if (CommonUtil.isEmpty(redis_key)) {
 			redisUtils.set("redis_key", loginfoget.getUser_email());
 			users = 1;
 		} else {
 			try {
+				// 锁住当前线程
 				lock.lock();
+				// 判断用户人数是否超过1人
 				if (users < 1) {
+					// 没有超过则登录
 					redisUtils.addUser("redis_key", loginfoget.getUser_email());
 					users = redis_key.split(",").length;
 					return;
 				}
+				// 判断当前人数是否超过1人
 				while (users >= 1) {
+					// 等待2秒
 					lock.newCondition().await(2, TimeUnit.SECONDS);
+					// 如果没有超过则登录
 					if (users < 1) {
 						redisUtils.addUser("redis_key", loginfoget.getUser_email());
 						users = redis_key.split(",").length;
@@ -50,20 +58,14 @@ public class CommonServiceImpl {
 						return;
 					}
 				}
+				// 唤醒线程
 				lock.newCondition().signal();
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
+				// 释放锁
 				lock.unlock();
 			}
 		}
-
-	}
-
-	public boolean getUsers() {
-		if (users >= 1) {
-			getUsers();
-		}
-		return true;
 	}
 }
