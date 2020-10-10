5c9761378617e7e647e883c1d3a211738d3e5852
wyx <dhc@DESKTOP-JUIGH46>
2020-10-10 15:00:07 +0900

    1

---
+++ src/main/java/com/amtf/demo/user/WebSockerUsers.java
@@ -0,0 +1,19 @@
+package com.amtf.demo.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WebSockerUsers {
+
+	/**
+	 * 登录用户
+	 */
+	private String userid;
+
+	/**
+	 * 登录用户头像
+	 */
+	private String userpath;
+}
