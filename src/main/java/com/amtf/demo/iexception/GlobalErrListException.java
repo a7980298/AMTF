ccff5bc08fe085d700fc2eafd90c5d2740fef0ac
Your Name <email@example.com>
2020-11-04 18:32:44 +0800

    管理页面 通知优化

---
+++ src/main/java/com/amtf/demo/iexception/GlobalErrListException.java
@@ -0,0 +1,51 @@
+package com.amtf.demo.iexception;
+
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.amtf.demo.exception.ErrListException;
+import com.amtf.demo.exception.SuccessException;
+import com.amtf.demo.util.CommonUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+@ResponseBody
+public class GlobalErrListException {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ErrListException.class)
+	public ModelAndView passwordMistake(ErrListException e) throws JSONException {
+		log.error("错误信息：" + e.getMessage());
+		ModelAndView mav = new ModelAndView();
+		String IViewId = e.getIViewId();
+		mav.addObject(IViewId + "Params", e.getEntity());
+		if (!CommonUtil.isEmpty(IViewId)) {
+			if (IViewId.equals("f010001")) {
+				IViewId = "/login";
+			} else if (IViewId.equals("f010002")) {
+				IViewId = "/main";
+			} else if (IViewId.equals("f020001")) {
+				IViewId = "/dashboard";
+			}
+		}
+		mav.setViewName(IViewId);
+		mav.addObject("codemsg", e.getMessage());
+		return mav;
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(SuccessException.class)
+	public ModelAndView getSuccessException(SuccessException e) throws JSONException {
+		log.error("错误信息：" + e.getMessage());
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("success", e.getMessage());
+		return mav;
+	}
+}
