9d6b41f76d1b89fa112111a400d01b99c6bb0d1f
Your Name <email@example.com>
2020-11-05 18:12:54 +0800

    管理页面 活动发布修改

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -292,7 +292,31 @@ function getObjectURL(file) {
 	}
 	return url ;
 }
+// 手机端判断
+function isMobile() {
+	var userAgentInfo = navigator.userAgent;
 
+	var mobileAgents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+
+	var mobile_flag = false;
+
+	//根据userAgent判断是否是手机
+	for (var v = 0; v < mobileAgents.length; v++) {
+		if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+			mobile_flag = true;
+			break;
+		}
+	}
+	var screen_width = window.screen.width;
+	var screen_height = window.screen.height;
+
+	//根据屏幕分辨率判断是否是手机
+	if (screen_width < 500 && screen_height < 800) {
+		mobile_flag = true;
+	}
+
+	return mobile_flag;
+}
 //日历控件生成
 $(document).ready(function() {
 	md.initFullCalendar();
