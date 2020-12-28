5aad68ed4f30f7750cd7916a76482e4447c91f45
a7980298 <1220875539@qq.com>
2020-12-28 22:34:46 +0800

    管理页面修改

--- src/main/java/com/amtf/demo/controller/F020001ControllerVue.java
+++ src/main/java/com/amtf/demo/controller/F020001ControllerVue.java
@@ -6,10 +6,13 @@ import com.amtf.demo.entityout.F020001EntityOut;
 import com.amtf.demo.params.F020001Params;
 import com.amtf.demo.service.F020001Service;
 import com.amtf.demo.util.ParameterUtil;
+import com.amtf.demo.util.StringUtil;
 import com.amtf.demo.util.ValiDationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class F020001ControllerVue extends ValiDationUtil {
@@ -47,8 +50,9 @@ public class F020001ControllerVue extends ValiDationUtil {
 	 */
 	@PostMapping("/Vf020001/getUser")
 	@ResponseBody
-	public Object f020001T001(F020001Params params, Model model) {
-
+	public Object f020001T001(@RequestBody Map map, Model model) {
+		F020001Params params = new F020001Params();
+		params.setUserId(StringUtil.toStr(map.get("userId")));
 		F020001EntityIn entityin = new F020001EntityIn();
 
 		ParameterUtil.copyParameter(entityin, params);
