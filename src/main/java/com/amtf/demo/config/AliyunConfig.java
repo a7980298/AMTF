0e702ca58b9f464d607bb6b1c1da6dfa830f400a
a7980298 <1220875539@qq.com>
2021-01-10 13:52:41 +0800

    管理页面修改

---
+++ src/main/java/com/amtf/demo/config/AliyunConfig.java
@@ -0,0 +1,29 @@
+package com.amtf.demo.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * oss
+ */
+@Configuration
+@PropertySource(value = {"classpath:application.properties"})
+@ConfigurationProperties(prefix = "aliyun")
+@Data
+public class AliyunConfig {
+	private String endpoint;
+	private String accessKeyId;
+	private String accessKeySecret;
+	private String bucketName;
+	private String urlPrefix;
+
+	@Bean
+	public OSS oSSClient() {
+		return new OSSClient(endpoint, accessKeyId, accessKeySecret);
+	}
+}
